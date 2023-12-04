b5d4c8bc3e29f2227d27d30b04033727f65196c7
Thiago Mota <thiagomotape@hotmail.com.br>
2023-12-04 17:58:40 -0300

    segundo componente

--- homes/src/app/app.component.ts
+++ homes/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { HousingLocationComponent } from './components/housing-location/housing-location.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HomeComponent],
+  imports: [CommonModule, RouterOutlet, HomeComponent, HousingLocationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
