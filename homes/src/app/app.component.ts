8bb20ffb53c869a81080a5be3e92d6d969d8368f
Thiago Mota <thiagomotape@hotmail.com.br>
2023-12-05 13:13:56 -0300

    mostrando dados
    mostrando dados de forma dinâmica passando os dados do componente pai para o componente filho através de interfaces

--- homes/src/app/app.component.ts
+++ homes/src/app/app.component.ts
@@ -3,13 +3,25 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { HousingLocationComponent } from './components/housing-location/housing-location.component';
+import { Housinglocation } from './interfaces/housinglocation';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet, HomeComponent, HousingLocationComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: './app.component.html'
 })
 
-export class AppComponent {}
+export class AppComponent {
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
+  housingLocation: Housinglocation = {
+    id: 0,
+    name: 'Ubaias',
+    city: 'Recife',
+    state: 'Pernambuco',
+    photo: '${this.baseUrl}/example-house.jpg',
+    availableUnits: 99,
+    wifi: true,
+    laundry: false
+  }
+}
